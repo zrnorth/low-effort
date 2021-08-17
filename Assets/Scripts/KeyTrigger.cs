8c702ad8b37f1af5b521744adcc9bb71377e9a61
Zach North <zrnorth@gmail.com>
2021-08-17 01:06:01 -0400

    Added stupid vfx

--- Assets/Scripts/KeyTrigger.cs
+++ Assets/Scripts/KeyTrigger.cs
@@ -8,31 +8,31 @@ public class KeyTrigger : MonoBehaviour
     public KeyCode triggerKey;
     private GameObject currentlyCollidingNote = null;
 
-    void Update()
-    {
-        if (currentlyCollidingNote != null && Input.GetKeyDown(triggerKey))
-        {
+    private ParticleSystem fxOnTrigger;
+
+    void Start() {
+        fxOnTrigger = GetComponent<ParticleSystem>();
+    }
+
+    void Update() {
+        if (currentlyCollidingNote != null && Input.GetKeyDown(triggerKey)) {
+            fxOnTrigger.Play();
             Destroy(currentlyCollidingNote);
             currentlyCollidingNote = null;
         }
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Note")
-        {
-            if (currentlyCollidingNote != null)
-            {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Note") {
+            if (currentlyCollidingNote != null) {
                 Debug.LogError("Overlapping notes");
                 return; // Don't thrash
             }
             currentlyCollidingNote = other.gameObject;
         }
     }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Note")
-        {
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Note") {
             currentlyCollidingNote = null;
         }
     }
