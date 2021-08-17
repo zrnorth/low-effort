7711b2f6eed511c0fc4f88fc6f406e57571c60f7
Zach North <zrnorth@gmail.com>
2021-08-16 23:50:57 -0400

    Adding KeyTrigger

--- Assets/Scripts/KeyTrigger.cs
+++ Assets/Scripts/KeyTrigger.cs
@@ -5,10 +5,37 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class KeyTrigger : MonoBehaviour
 {
+    public KeyCode triggerKey;
+    private GameObject currentlyCollidingNote = null;
+
+    void Update()
+    {
+        if (currentlyCollidingNote != null && Input.GetKeyDown(triggerKey))
+        {
+            Destroy(currentlyCollidingNote);
+            currentlyCollidingNote = null;
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag != "Note") return;
-
-        Debug.Log("Triggered by a note");
+        if (other.gameObject.tag == "Note")
+        {
+            if (currentlyCollidingNote != null)
+            {
+                Debug.LogError("Overlapping notes");
+                return; // Don't thrash
+            }
+            currentlyCollidingNote = other.gameObject;
+        }
     }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Note")
+        {
+            currentlyCollidingNote = null;
+        }
+    }
+
+
 }
