b34d81055b5cd556a1278328233ed213c3609d6f
Steven La <mrstevenla@gmail.com>
2021-08-17 00:51:44 -0400

    made gamemanager read notes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+
+    [SerializeField]
+    private TextAsset chart;
+
     [SerializeField]
     private GameObject noteBoard;
+
+    [SerializeField]
+    private Material[] noteMaterials;
+
+    [SerializeField]
+    private float[] noteXPositions = { -1.8f, -0.6f, 0.6f, 1.8f };
+
+    [SerializeField]
+    private GameObject notePrefab;
 
     [Header("DEBUG")]
     [SerializeField]
@@ -14,18 +28,37 @@ public class GameManager : Singleton<GameManager>
     [SerializeField]
     private bool debugExecuteTimer = false;
 
-    void Start() {
+    private MovingBillboard billboard;
 
+    void Start() {
+        billboard = noteBoard.GetComponent<MovingBillboard>();
+
+        string[] chartLines = Regex.Split(chart.text, "\r\n|\r|\n");
+        foreach (string chartLine in chartLines) {
+            string[] chartData = Regex.Split(chartLine, "\\s");
+            string timeRaw = chartData[0];
+            string[] timeParts = timeRaw.Split(':');
+            float time = System.Convert.ToSingle(timeParts[0]) * 60 + System.Convert.ToSingle(timeParts[1]);
+            int note = System.Convert.ToInt32(chartData[1]);
+            Vector3 position = new Vector3(
+                noteXPositions[note],
+                0f,
+                0f + billboard.movement.z * time * -1
+            );
+            GameObject obj = Instantiate(notePrefab, position, Quaternion.identity);
+            obj.transform.parent = noteBoard.transform;
+            Transform renderer = obj.transform.GetChild(0);
+            renderer.gameObject.GetComponent<MeshRenderer>().material = noteMaterials[note];
+        }
     }
 
     // Update is called once per frame
     void Update() {
         if (debugExecuteTimer) {
-            MovingBillboard movingBillboard = noteBoard.GetComponent<MovingBillboard>();
             noteBoard.transform.position = new Vector3(
-                movingBillboard.initialPosition.x + movingBillboard.movement.x * debugSetTimeElapsed,
-                movingBillboard.initialPosition.y + movingBillboard.movement.y * debugSetTimeElapsed,
-                movingBillboard.initialPosition.z + movingBillboard.movement.z * debugSetTimeElapsed
+                billboard.initialPosition.x + billboard.movement.x * debugSetTimeElapsed,
+                billboard.initialPosition.y + billboard.movement.y * debugSetTimeElapsed,
+                billboard.initialPosition.z + billboard.movement.z * debugSetTimeElapsed
             );
             AudioManager.Instance.SetTime(debugSetTimeElapsed);
             debugExecuteTimer = false;
